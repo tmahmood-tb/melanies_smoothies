8178d51de480e49cca0027005b62dee7b978532e
tmahmood-tb <talat.mahmood@trinitybridge.com>
2026-07-30 13:52:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customise Your Smoothie :cup_with_straw:")
 
