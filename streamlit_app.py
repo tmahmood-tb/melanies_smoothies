51dc71b6599b7dfab9d8e8c4cd53d3c8472df7d7
tmahmood-tb <talat.mahmood@trinitybridge.com>
2026-07-30 14:44:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,5 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/Blueberry")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
